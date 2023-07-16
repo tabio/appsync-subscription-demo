3d8f9c1d59228a7f7d831e938ebbf3c7e432204e
tabio <tabio.github@gmail.com>
2023-07-16 21:29:38 +0900

    cdk init

---
+++ test/appsync-subscription-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AppsyncSubscriptionDemo from '../lib/appsync-subscription-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-subscription-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppsyncSubscriptionDemo.AppsyncSubscriptionDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
